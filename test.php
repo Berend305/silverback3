bd043c9d07c7bebb15bfc37ef5660fd1ff275dc6
berend <74530848+Berend305@users.noreply.github.com>
2021-03-19 11:45:46 +0100

    versie 2 Berend

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+$Berend = "Berend Beerendonk";
+echo $Berend;
+
+$voornaam = "Rene";
+$tussenvoegsel = "van";
+$achternaam = "Keulen"
+
+?>
+
+
+Rene van Keulen.
+tip: op een regel en concateneren die handel!!
